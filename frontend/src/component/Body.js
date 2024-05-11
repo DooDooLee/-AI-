98dcb28f223fa8c162ca4a79ba5ce70240542b33
regrevvv <txr0317@gmail.com>
2024-05-12 05:46:20 +0900

    feature : 메인페이지 CSS 추가 및 파일 정리

--- frontend/src/component/Body.js
+++ frontend/src/component/Body.js
@@ -1,20 +1,15 @@
 import Poster from "./body/Poster";
-//import BestbookContainer from "./body/BestbookContainer";
-//import FAQs from "./body/FAQContainer";
-//import NewbookContainer from "./body/NewbookContainer";
+import BestbookContainer from "./body/BestbookContainer";
+import NewbookContainer from "./body/NewbookContainer";
 
 function Body() {
     return (
         <>
             <Poster />
+            <BestbookContainer />
+            <NewbookContainer />
         </>
     );
 }
 
 export default Body;
-
-/*
-            <BestbookContainer />
-            <NewbookContainer />
-            <FAQs />
-*/

755152e6d8b6dcb1ceacea8a969fdc3f86957620
SHSong <99pyues2@gmail.com>
2024-05-11 18:27:15 +0900

    도메인 개발

---
+++ backend/src/main/java/com/bigPicture/backend/domain/Review.java
@@ -0,0 +1,38 @@
+package com.bigPicture.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Review {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @CreatedDate
+    private LocalDateTime createdAd;
+
+    private String contents;
+
+}

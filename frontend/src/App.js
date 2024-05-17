6e732163c6dc0416eae052a353586309a64c2113
regrevvv <txr0317@gmail.com>
2024-05-17 21:05:21 +0900

    좌측패널 html구성 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import MainPage from "./pages/MainPage.js";
+import BookMaker from "./pages/BookMaker.js";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<MainPage />} />
+            <Route path="/BookMaker" element={<BookMaker />} />
         </Routes>
     );
 }
---
+++ frontend/src/pages/BookMaker.js
@@ -0,0 +1,14 @@
+import Header from "../component/Header";
+import LeftPanel from "../component/LeftPanel";
+import styles from "../styles/BookMaker.module.css";
+
+function BookMaker() {
+    return (
+        <>
+            <Header />
+            <LeftPanel />
+        </>
+    );
+}
+
+export default BookMaker;

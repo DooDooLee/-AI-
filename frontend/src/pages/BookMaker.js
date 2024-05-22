3e4606143f8fdcf25268a72b2ce9e28d61c11918
이승엽 <dltmdduq8484@gmail.com>
2024-05-23 03:45:19 +0900

    css

--- frontend/src/pages/BookMaker.js
+++ frontend/src/pages/BookMaker.js
@@ -2,6 +2,8 @@ import Header from "../component/Header";
 import LeftPanel from "../component/LeftPanel";
 import PromptContainer from "../component/PromptComtainer";
 import styles from "../styles/BookMaker.module.css";
+import { useHistory } from 'react-router-dom';
+
 
 function BookMaker() {
     return (

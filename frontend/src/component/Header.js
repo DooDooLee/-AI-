ea829e8bfdf2e09107e676b21a26daca67d340e7
이승엽 <126196812+DooDooLee@users.noreply.github.com>
2024-05-23 01:47:47 +0900

    Feature/#41 책생성 페이지 백엔드 기능 연동 (#48)
    * 이미지 생성/책생성 DB연동까지 구현
    
    * 백엔드 클래스 파일명 에러수정
    
    * 이미지 생성이나 글생성이 되어야 다음페이지가 되도록 수정/ 현재 페이지 넘버 표시되도록 함
    
    * 세션 스토리지에 토큰이 저장안되는 오류 해결
    
    * 이미지 요청 api 수정
    
    * 헤더수정

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,94 +1,113 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import styles from "../styles/Header.module.css";
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from '../styles/Header.module.css';
 
 function Header() {
-    const logo_uri = "./public_assets/logo.svg";
-    const search_uri = "./public_assets/search.svg";
-    const menu_uri = "./public_assets/menu.svg";
-    const [userName, setUserName] = useState(null); // 유저 이름 상태 추가
+  const logo_uri = './public_assets/logo.svg';
+  const search_uri = './public_assets/search.svg';
+  const menu_uri = './public_assets/menu.svg';
+  const [userName, setUserName] = useState(null);
+  const navigate = useNavigate();
 
-    // 로그인 버튼을 눌렀을 때 처리하는 함수
-    const handleLogin = () => {
-        // 현재 페이지 URL을 가져옵니다.
-        const redirectUri = encodeURIComponent(window.location.href);
-        // 백엔드로 리다이렉트하며, 리다이렉트 후 다시 현재 페이지로 돌아올 수 있도록 현재 페이지 URL을 함께 전달합니다.
-        window.location.href = `http://localhost:8080/oauth2/authorization/kakao?redirect_uri=${redirectUri}`;
-    };
+  const handleLogin = () => {
+    const redirectUri = encodeURIComponent(window.location.href);
+    window.location.href = `http://localhost:8080/oauth2/authorization/kakao?redirect_uri=${redirectUri}`;
+  };
 
-    // 사용자 정보를 가져오는 함수
-    const fetchUserInfo = (token) => {
-        fetch("http://localhost:8080/user/me", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`, // Bearer 토큰 형식으로 헤더에 토큰 추가
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setUserName(data.name); // 사용자 이름 설정
-            })
-            .catch((error) =>
-                console.error("사용자 정보를 가져오는 동안 에러 발생:", error)
-            );
-    };
+  const handleLogout = () => {
+    // 세션 스토리지 비우기
+    sessionStorage.removeItem('authToken');
+    sessionStorage.removeItem('userName');
+    // 사용자 이름 상태 업데이트
+    setUserName(null);
+    // 메인 페이지로 이동
+    navigate('/');
+  };
 
-    // 페이지가 로드될 때 세션 스토리지에서 토큰을 확인하고 사용자 정보를 가져옵니다.
-    useEffect(() => {
-        const token = sessionStorage.getItem("authToken");
-        if (token) {
-            fetchUserInfo(token);
-        }
-    }, []);
+  useEffect(() => {
+    // 현재 URL에서 토큰을 가져옴
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    if (token) {
+      handleLoginSuccess(token);
+      // URL에서 토큰 제거
+      window.history.replaceState({}, document.title, window.location.pathname);
+    } else {
+      const storedToken = sessionStorage.getItem('authToken');
+      const storedUserName = sessionStorage.getItem('userName');
+      if (!storedUserName && storedToken) {
+        fetchUserInfo(storedToken);
+      } else {
+        setUserName(storedUserName);
+      }
+    }
+  }, []);
 
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.left}>
-                <Link to="/" id="logo">
-                    <img src={logo_uri} alt="logo" />
-                </Link>
-                <div>
-                    <ul>
-                        <Link to="/BookMaker">
-                            <li className={styles.listItem}>책 만들기</li>
-                        </Link>
-                        <li className={styles.listItem}>P-Book 도서관</li>
-                        <li className={styles.listItem}>커뮤니티</li>
-                    </ul>
-                </div>
-            </div>
-            <div className={styles.right}>
-                <ul>
-                    {userName ? (
-                        <li className={styles.userName}>{userName} 님</li> // 사용자 이름 표시
-                    ) : (
-                        <li
-                            className={styles.loginButton}
-                            onClick={handleLogin}
-                        >
-                            로그인
-                        </li> // 로그인 버튼
-                    )}
-                    <li>
-                        <img
-                            width="30px"
-                            height="30px"
-                            src={search_uri}
-                            alt="검색"
-                        />
-                    </li>
-                    <li>
-                        <img
-                            width="30px"
-                            height="30px"
-                            src={menu_uri}
-                            alt="메뉴"
-                        />
-                    </li>
-                </ul>
-            </div>
+  const handleLoginSuccess = (token) => {
+    sessionStorage.setItem('authToken', token);
+    fetchUserInfo(token);
+  };
+
+  const fetchUserInfo = (token) => {
+    fetch('http://localhost:8080/user/me', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        sessionStorage.setItem('userName', data.name);
+        setUserName(data.name);
+      })
+      .catch((error) =>
+        console.error('사용자 정보를 가져오는 동안 에러 발생:', error)
+      );
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.left}>
+        <Link to="/" id="logo">
+          <img src={logo_uri} alt="logo" />
+        </Link>
+        <div>
+          <ul>
+            <li
+              className={styles.listItem}
+              onClick={() => navigate('/BookMaker')}
+            >
+              책 만들기
+            </li>
+            <li className={styles.listItem}>P-Book 도서관</li>
+            <li className={styles.listItem}>커뮤니티</li>
+          </ul>
         </div>
-    );
+      </div>
+      <div className={styles.right}>
+        <ul>
+          {userName ? (
+            <>
+              <li className={styles.userName}>{userName} 님</li>
+              <li className={styles.logoutButton} onClick={handleLogout}>
+                로그아웃
+              </li>
+            </>
+          ) : (
+            <li className={styles.loginButton} onClick={handleLogin}>
+              로그인
+            </li>
+          )}
+          <li>
+            <img width="30px" height="30px" src={search_uri} alt="검색" />
+          </li>
+          <li>
+            <img width="30px" height="30px" src={menu_uri} alt="메뉴" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- frontend/src/component/PromptComtainer.js
+++ frontend/src/component/PromptComtainer.js
@@ -1,124 +1,246 @@
-import { useState } from "react";
-import styles from "../styles/PromptContainer.module.css";
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/PromptContainer.module.css';
+import { useNavigate } from 'react-router-dom';
 
-function PromptContainer() {
-    const [makingCover, setMakingCover] = useState(true); //true일 시 표지 제작창
-    const [prompt, setPrompt] = useState(""); //프롬프트 내용
-    const [referenceDegree, setReferenceDegree] = useState(10); //프롬프트 참조 정도
-    const [generatedImage, setGeneratedImage] = useState(""); //생성된 이미지
-    const [bookTitle, setBookTitle] = useState(""); //책 제목
-    const [keepSeed, setKeepSeed] = useState(true); //시드 유지 여부
-    const [passage, setPassage] = useState(""); //책 내용
+const PromptContainer = () => {
+  const [referenceDegree, setReferenceDegree] = useState(0);
+  const [prompt, setPrompt] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [generatedImage, setGeneratedImage] = useState('');
+  const [pages, setPages] = useState([
+    { pageNumber: 0, image: '', content: '' },
+  ]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [seed, setSeed] = useState('');
+  const navigate = useNavigate(); // useNavigate 훅 사용
 
-    const onSubmitClick = (event) => {
-        //이미지 생성버튼 콜백함수
-        event.preventDefault();
-        /*
-        이미지 받아오기
-        */
-        setGeneratedImage("");
-    };
+  useEffect(() => {
+    setPages([{ pageNumber: 0, image: '', content: '' }]);
+    setCurrentIndex(0);
+  }, []);
 
-    const onNextPageClick = (e) => {
-        if (generatedImage === "") {
-            //이미지가 생성되지 않았을 경우
-            alert("아직 이미지가 생성되지 않았습니다.");
-            return;
-        }
-        if (makingCover === true) {
-            if (bookTitle === "") {
-                alert("책 제목을 입력해 주세요.");
-                return;
-            }
-            setMakingCover(false);
-            setPrompt(""); //표지 생성 후 프롬프트 지우기
-        }
-        /*
-        생성된 이미지 처리
-        */
-    };
-    return (
-        <div id="wrapper" className={styles.wrapper}>
-            <div id="left" className={styles.left}>
-                <img src={generatedImage} alt="생성된 이미지" />
-            </div>
-            <form id="right" className={styles.right}>
-                <div id="upperForm" className={styles.upperForm}>
-                    <div id="prompt" className={styles.prompt}>
-                        <input
-                            type="text"
-                            value={prompt}
-                            placeholder={`${
-                                makingCover ? "표지" : ""
-                            } 이미지 생성을 위한 프롬프트를 작성해주세요.`}
-                            onChange={(e) => {
-                                setPrompt(e.target.value);
-                            }}
-                        />
-                    </div>
-                    <div id="option" className={styles.option}>
-                        <span>프롬프트 참조 정도</span>
-                        {referenceDegree}
-                        <br />
-                        <input
-                            type="range"
-                            min={0}
-                            max={20}
-                            value={referenceDegree}
-                            onChange={(e) => {
-                                setReferenceDegree(parseInt(e.target.value));
-                            }}
-                        />
-                        <br />
-                        <span>
-                            시드 유지
-                            <input
-                                type="checkbox"
-                                checked={keepSeed}
-                                onChange={(e) => {
-                                    setKeepSeed(!keepSeed);
-                                }}
-                            />
-                        </span>
-                        <br />
-                        <button type="submit" onClick={onSubmitClick}>
-                            이미지 생성
-                        </button>
-                    </div>
-                </div>
-                {makingCover ? ( //최초 렌더링 시 책 제목 입력창 렌더링.
-                    <input
-                        type="text"
-                        value={bookTitle}
-                        onChange={(e) => {
-                            setBookTitle(e.target.value);
-                        }}
-                        className={styles.bookTitle}
-                        placeholder="책 제목을 입력해주세요."
-                    />
-                ) : (
-                    <textarea
-                        value={passage}
-                        onChange={(e) => {
-                            setPassage(e.target.value);
-                        }}
-                        placeholder="글 내용을 입력해주세요..."
-                    />
-                )}
-            </form>
-            <div id="buttonArea" className={styles.buttonArea}>
-                <div>
-                    <button>임시 저장</button>
-                </div>
-                <div>
-                    <button onClick={onNextPageClick}>다음 페이지</button>
-                    {
-                        makingCover !== true && <button>제작 완성</button> //표지 완성 후에만 렌더링
-                    }
-                </div>
-            </div>
-        </div>
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const token = sessionStorage.getItem('authToken');
+      const response = await fetch('http://localhost:8080/image', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ prompt: prompt, seed: seed || undefined }),
+      });
+      const data = await response.json();
+      setTimeout(() => {
+        setGeneratedImage(data.imageUrl);
+        setLoading(false);
+
+        // 시드 값을 입력 폼에 설정
+        setSeed(data.seed.toString());
+
+        // 현재 인덱스의 페이지 정보 업데이트
+        setPages((prevPages) => {
+          const newPages = [...prevPages];
+          newPages[currentIndex] = {
+            ...newPages[currentIndex],
+            image: data.imageUrl,
+            seed: data.seed,
+          };
+          console.log('Updated Pages:', newPages); // 페이지 배열 로그 출력
+          return newPages;
+        });
+      }, 3000);
+    } catch (error) {
+      console.error('Error:', error);
+      setLoading(false);
+    }
+  };
+
+  const handleShowImage = () => {
+    setGeneratedImage(
+      (prevUrl) => prevUrl + '?timestamp=' + new Date().getTime()
     );
-}
+  };
+
+  const handleNextPage = () => {
+    const currentPage = pages[currentIndex];
+    if (!currentPage.content && !currentPage.image) {
+      alert('글 내용이나 이미지를 입력해주세요.');
+      return; // 다음 페이지로 이동하지 않고 함수 종료
+    }
+
+    // 현재 페이지가 마지막 페이지인지 확인하고, 마지막 페이지가 아니라면 다음 페이지로 이동
+    setPages((prevPages) => {
+      const newPages = [...prevPages];
+      if (currentIndex < prevPages.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        newPages.push({ pageNumber: currentIndex + 1, image: '', content: '' });
+        setCurrentIndex(currentIndex + 1);
+      }
+      console.log('Updated Pages:', newPages); // 페이지 배열 로그 출력
+      setGeneratedImage(''); // 다음 페이지로 이동 시 이미지 초기화
+      return newPages;
+    });
+  };
+
+  const handleContentChange = (e) => {
+    const newContent = e.target.value;
+    setPages((prevPages) => {
+      const newPages = [...prevPages];
+      newPages[currentIndex] = {
+        ...newPages[currentIndex],
+        content: newContent,
+      };
+      return newPages;
+    });
+  };
+
+  const handleCreateBook = async () => {
+    const token = sessionStorage.getItem('authToken');
+
+    // 유효한 페이지만 필터링
+    const validPages = pages.filter((page) => page.image || page.content);
+
+    // 유효한 페이지가 하나도 없는 경우 알림
+    if (validPages.length === 0) {
+      alert('최소 한 페이지는 입력을 마쳐야 합니다.');
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:8080/book/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          title: 'Your Book Title',
+          cover: 'Cover Image URL',
+          pages: validPages,
+        }),
+      });
+      if (response.ok) {
+        alert('제작이 완료되었습니다.');
+        navigate('/');
+      } else {
+        console.error('Error creating book');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div id="wrapper" className={styles.wrapper}>
+      <div id="left" className={styles.left}>
+        {loading && <p style={{ marginTop: '10px' }}>로딩 중...</p>}
+        {!loading && generatedImage && (
+          <img
+            src={generatedImage}
+            alt="생성된 이미지"
+            style={{
+              maxWidth: '750px',
+              maxHeight: '550px',
+              width: 'auto',
+              height: 'auto',
+              borderRadius: '20px',
+            }}
+          />
+        )}
+        <span
+          style={{
+            position: 'absolute',
+            bottom: '20px',
+            right: '1340px',
+            color: 'black',
+            padding: '5px 10px',
+            borderRadius: '5px',
+            fontSize: '35px',
+            zIndex: 1,
+          }}
+        >
+          {currentIndex + 1}
+        </span>
+      </div>
+      <form id="right" className={styles.right}>
+        <div id="upperForm" className={styles.upperForm}>
+          <div id="prompt" className={styles.prompt}>
+            <input
+              type="text"
+              name="prompt"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              placeholder="이미지 생성을 위한 프롬프트를 입력해 주세요"
+            />
+          </div>
+          <div id="option" className={styles.option}>
+            <span>프롬프트 참조 정도</span>
+            {referenceDegree}
+            <br />
+            <input
+              type="range"
+              min={0}
+              max={20}
+              value={referenceDegree}
+              onChange={(e) => setReferenceDegree(e.target.value)}
+            />
+            <br />
+            <span>시드 값</span>
+            <input
+              type="text"
+              value={seed}
+              onChange={(e) => setSeed(e.target.value)}
+              placeholder="시드 값을 입력해 주세요"
+            />
+            <br />
+            <button type="button" onClick={handleSubmit} disabled={loading}>
+              이미지 생성
+            </button>
+            {generatedImage && (
+              <button
+                type="button"
+                onClick={handleShowImage}
+                disabled={loading}
+              >
+                이미지 다시 표시
+              </button>
+            )}
+          </div>
+        </div>
+        <textarea
+          placeholder="글 내용을 입력해주세요"
+          value={pages[currentIndex]?.content || ''}
+          onChange={handleContentChange}
+        />
+      </form>
+      <div id="buttonArea" className={styles.buttonArea}>
+        <div>
+          <button id="tempSaveBtn" className={styles.tempSaveBtn}>
+            임시 저장
+          </button>
+        </div>
+        <div>
+          <button
+            id="nextPageBtn"
+            className={styles.rightBtn}
+            onClick={handleNextPage}
+          >
+            다음 페이지
+          </button>
+          <button
+            id="completeBtn"
+            className={styles.rightBtn}
+            onClick={handleCreateBook}
+          >
+            제작 완성
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PromptContainer;

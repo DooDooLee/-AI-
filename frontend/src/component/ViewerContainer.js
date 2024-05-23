9c13bacae493b8991ce47103843164931b793b05
regrevvv <txr0317@gmail.com>
2024-05-23 22:01:34 +0900

    피그마 변경 이후 수정점 중간저장

--- frontend/src/component/ViewerContainer.js
+++ frontend/src/component/ViewerContainer.js
@@ -1,14 +1,28 @@
-import BookPageComponent from "./bookviewer/BookPageComponent";
+import { useState } from "react";
+
+import styles from "../styles/ViewerContainer.module.css";
 
 function BookViewer() {
-    const prevPage = () => {};
-    const nextPage = () => {};
+    /*
+     */
+    const [currentPage, setCurrentPage] = useState({
+        image: "",
+        content: "",
+        pageNumber: -1,
+    });
+
+    const prevPageClick = () => {};
+    const nextPageClick = () => {};
     return (
-        <div>
-            <BookPageComponent />
-            <BookPageComponent />
-            <button onClick={prevPage}>이전 페이지</button>
-            <button onClick={nextPage}>다음 페이지</button>
+        <div className={styles.wrapper}>
+            <div className={styles.left}>
+                <img src={currentPage.image} alt="페이지 이미지"></img>
+            </div>
+            <div className={styles.right}>
+                <p>{currentPage.content}</p>
+            </div>
+            <button onClick={prevPageClick}>이전 페이지</button>
+            <button onClick={nextPageClick}>다음 페이지</button>
         </div>
     );
 }

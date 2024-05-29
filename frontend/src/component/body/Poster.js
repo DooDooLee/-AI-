b9fe6d5bbaf39e07f26c170bdcd009114cdd2e84
wl-dnjs <164969273+wl-dnjs@users.noreply.github.com>
2024-05-30 01:32:46 +0900

    포스터, 소개 (#75)

--- frontend/src/component/body/Poster.js
+++ frontend/src/component/body/Poster.js
@@ -1,10 +1,10 @@
 import styles from "../../styles/Poster.module.css";
+import PosterImage from "../../images/PosterImage.png";
 
 function Poster() {
-    const poster_default_uri = "./public_assets/poster_default.svg";
     return (
         <div className={styles.wrapper}>
-            <img src={poster_default_uri} alt="default_poster"></img>
+            <img src={PosterImage} alt="=poster"></img>
         </div>
     );
 }

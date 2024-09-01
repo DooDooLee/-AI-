cdc0538ecc14a22db8bf51825c35bc3c12bcf49d
SHSong <99pyues2@gmail.com>
2024-09-01 15:02:26 +0900

    feature : 내가 작성한 책 불러오기(페이지네이션)

--- backend/src/main/java/com/bigPicture/backend/repository/BookRepository.java
+++ backend/src/main/java/com/bigPicture/backend/repository/BookRepository.java
@@ -10,9 +10,7 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    @Query("SELECT b FROM Book b WHERE b.user.id = :userId")
-    List<Book> findBooksByUserId(@Param("userId") Long userId);
-
+    List<Book> findByUser(User user);
 
     List<Book> findAllByOrderByIdDesc();
 
--- backend/src/main/java/com/bigPicture/backend/service/UserBookService.java
+++ backend/src/main/java/com/bigPicture/backend/service/UserBookService.java
@@ -1,12 +1,16 @@
 package com.bigPicture.backend.service;
 
 import com.bigPicture.backend.domain.Book;
+import com.bigPicture.backend.domain.User;
 import com.bigPicture.backend.payload.response.BookInfoResponse;
 import com.bigPicture.backend.payload.response.UserBookInfoResponse;
 import com.bigPicture.backend.payload.response.UserBooksResponse;
 import com.bigPicture.backend.repository.BookRepository;
+import com.bigPicture.backend.repository.UserRepository;
+import com.bigPicture.backend.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,11 +20,18 @@ import java.util.stream.Collectors;
 public class UserBookService {
 
     private final BookRepository bookRepository;
+    private final UserRepository userRepository;
 
-    public List<UserBookInfoResponse> getAllUserBooks(Long userId) {
+    @Transactional
+    public List<UserBookInfoResponse> getAllUserBooks(UserPrincipal userPrincipal, int page, int size) {
+        int offset = (page - 1) * size;
+        User user = userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
 
-        List<Book> books = bookRepository.findBooksByUserId(userId);
+        List<Book> books = bookRepository.findByUser(user);
         return books.stream()
+                .skip(offset)
+                .limit(size)
                 .map(UserBookInfoResponse::of)
                 .collect(Collectors.toList()); //응답 데이터를 던져야 함으로 DTO 로 변환
     }
